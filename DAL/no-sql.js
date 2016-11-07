8b68077ce0db81f953ab9d4721442938521c00f8
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-11-07 09:31:13 -0500

    initial commit

---
+++ DAL/no-sql.js
@@ -0,0 +1,13 @@
+const dal = {
+    listShoes: listShoes,
+    getShoes: getShoes,
+    createShoes: updateShoes
+}
+
+function listShoes(sortBy, startKey, limit, callback) {}
+
+function getShoes(id, callback) {}
+
+function createShoes(data, callback) {}
+
+module.exports = dal
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # api-exam
-Test API Exam- Shoes
+##Test API Exam- Shoes
+
+#### This API will allow the user to access a database of shoes to see what shoes are in stock, when they are available, and their descriptions and price points. 
